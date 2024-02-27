4995f85a6846700b3d0bf93ef57341fb80a0134c
annashubskaya <anna.shubskaya@solbeg.com>
2024-02-27 11:37:18 +0300

    change num

--- src/layout/MainLayout/Header/HeaderContactUs.js
+++ src/layout/MainLayout/Header/HeaderContactUs.js
@@ -14,7 +14,7 @@ export function HeaderContactUs({options, activeMenu, setActiveMenu}) {
     return (
         <div className={styles.contactUs}>
             <span>Звоните Пн-Вс: 9:00 - 21:00</span>
-            <a href="tel:+3758000000">+375 (800)-00-00</a>
+            <a href="tel:+375297953512">+375 (29) 795-35-12</a>
         </div>
     )
 }
